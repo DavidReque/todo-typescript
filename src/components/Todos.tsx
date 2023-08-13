347801e3d316acf4155b6c331de0d678311c2a4c
David Requeno <davidrequeno52@gmail.com>
2023-08-13 11:05:01 -0600

    types

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,4 @@
-type Todo = {
-    id: string,
-	title: string,
-	completed: boolean
-}
-
-type ListOfTodos = Todo[]
+import {type ListOfTodos} from '../types'
 
 interface Props {
     todos: ListOfTodos
