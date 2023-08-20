164a5cc56d883dde31173fde118fa2c8b9733f1c
David Requeno <davidrequeno52@gmail.com>
2023-08-20 14:57:27 -0600

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import { FilterValue } from "../types"
+import { Filters } from "./Filters"
+
+interface Props {
+    activeCount: number
+    completedCount: number
+    handleRemove: () => void
+    filterSelected: FilterValue
+    handleFilterChange: (filter: FilterValue) => void
+}
+
+export const Footer: React.FC<Props> = ({
+    activeCount = 0, 
+    completedCount = 0,
+    filterSelected,
+    handleFilterChange,
+    handleRemove
+}) => {
+    return (
+        <footer className="footer">
+            <span className="todo-count">
+                <strong>{activeCount}</strong>Tareas pendientes
+            </span>
+
+            <Filters 
+            filterSelected={filterSelected}
+            onFilterChange={handleFilterChange} />
+        </footer>
+    )
+}
